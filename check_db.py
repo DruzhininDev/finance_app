34a423190ce802e86c6557b693a746e2a07b714c
Druzhinin Viktor <druzhinin.dev@gmail.com>
2026-09-19 21:27:15 +0300

    Removed check_db.py from repo

--- check_db.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-conn = sqlite3.connect("finance.db")
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM transactions")
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
-conn.close()
